e520307cb51ffee3785fcdacde0d2ef87d4740ec
JinwooSeong <163496499+JinwooSeong@users.noreply.github.com>
2024-09-11 20:48:23 +0900

    first commit

---
+++ firstproject/src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO article(title,content) VALUES ('가가가가','1111');
+INSERT INTO article(title,content) VALUES ('나나나나','2222');
+INSERT INTO article(title,content) VALUES ('다다다다','3333');
+
+INSERT INTO article(title,content) VALUES ('당신의 인생 영화는?','댓글 고');
+INSERT INTO article(title,content) VALUES ('당신의 소울 푸드는?','댓글 고고');
+INSERT INTO article(title,content) VALUES ('당신의 취미는?','댓글 고고고');
+
+INSERT INTO comment(article_id,nickname,body) VALUES (4,'park','굿윌 헌팅');
+INSERT INTO comment(article_id,nickname,body) VALUES (4,'kim','아이 엠 샘');
+INSERT INTO comment(article_id,nickname,body) VALUES (4,'choi','쇼생크 탈출');
+
+INSERT INTO comment(article_id,nickname,body) VALUES (5,'park','치킨');
+INSERT INTO comment(article_id,nickname,body) VALUES (5,'kim','샤브샤브');
+INSERT INTO comment(article_id,nickname,body) VALUES (5,'choi','초밥');
+
+INSERT INTO comment(article_id,nickname,body) VALUES (6,'park','조깅');
+INSERT INTO comment(article_id,nickname,body) VALUES (6,'kim','유튜브 시청');
+INSERT INTO comment(article_id,nickname,body) VALUES (6,'choi','독서');
